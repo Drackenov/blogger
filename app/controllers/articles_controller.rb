5e0478bad3b5c19f22d6d5c966651c70aad3d55c
drackenov <robertdraketalley@gmail.com>
2013-09-29 23:26:26 +0000

    tons o shit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,20 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		redirect_to article_path(@article)
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+
 	def show
 		@article = Article.find(params[:id])
 	end
